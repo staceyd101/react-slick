dbe6a900c2809db9ac7f1d3ed2b18fc6809c151e
Derek S <staceyd101@yahoo.com>
2022-02-12 19:17:22 -0800

    wip

--- src/track.js
+++ src/track.js
@@ -116,14 +116,24 @@ const renderSlides = spec => {
     let childStyle = getSlideStyle({ ...spec, index });
     let slideClass = child.props.className || "";
     let slideClasses = getSlideClasses({ ...spec, index });
+    const slidesAccessibility = {
+      ...(spec.showAccesibilityRole && {
+        "role": "group",
+        "aria-label": `slide ${index}`,
+      }),
+      ...(!slideClasses["slick-active"] && {
+        "aria-hidden": true,
+      }),
+      tabIndex: -1,
+    };
+
     // push a cloned element of the desired slide
     slides.push(
       React.cloneElement(child, {
         key: "original" + getKey(child, index),
         "data-index": index,
+        ...slidesAccessibility,
         className: classnames(slideClasses, slideClass),
-        tabIndex: "-1",
-        "aria-hidden": !slideClasses["slick-active"],
         style: { outline: "none", ...(child.props.style || {}), ...childStyle },
         onClick: e => {
           child.props && child.props.onClick && child.props.onClick(e);
@@ -146,13 +156,18 @@ const renderSlides = spec => {
           child = elem;
         }
         slideClasses = getSlideClasses({ ...spec, index: key });
+        preCloneSlidesAccessibility = {
+          ...(!slideClasses["slick-active"] && {
+            "aria-hidden": true,
+          }),
+          tabIndex: -1,
+        }
         preCloneSlides.push(
           React.cloneElement(child, {
             key: "precloned" + getKey(child, key),
             "data-index": key,
-            tabIndex: "-1",
             className: classnames(slideClasses, slideClass),
-            "aria-hidden": !slideClasses["slick-active"],
+            ...preCloneSlidesAccessibility,
             style: { ...(child.props.style || {}), ...childStyle },
             onClick: e => {
               child.props && child.props.onClick && child.props.onClick(e);
@@ -170,13 +185,18 @@ const renderSlides = spec => {
           child = elem;
         }
         slideClasses = getSlideClasses({ ...spec, index: key });
+        const postCloneSlidesAccessibility = {
+          ...(!slideClasses["slick-active"] && {
+            "aria-hidden": true,
+          }),
+          tabIndex: -1,
+        };
         postCloneSlides.push(
           React.cloneElement(child, {
             key: "postcloned" + getKey(child, key),
             "data-index": key,
-            tabIndex: "-1",
             className: classnames(slideClasses, slideClass),
-            "aria-hidden": !slideClasses["slick-active"],
+            ...postCloneSlidesAccessibility,
             style: { ...(child.props.style || {}), ...childStyle },
             onClick: e => {
               child.props && child.props.onClick && child.props.onClick(e);
@@ -206,14 +226,23 @@ export class Track extends React.PureComponent {
 
   render() {
     const slides = renderSlides(this.props);
-    const { onMouseEnter, onMouseOver, onMouseLeave } = this.props;
+    const { onMouseEnter, onMouseOver, onMouseLeave, accesibilityRegion } = this.props;
     const mouseEvents = { onMouseEnter, onMouseOver, onMouseLeave };
+    const accessibility = {
+      ...(!!accesibilityRegion && { 
+        role: "region", 
+        "aria-label": accesibilityRegion,
+        tabIndex: -1
+      })
+    };
+    
     return (
       <div
         ref={this.handleRef}
         className="slick-track"
         style={this.props.trackStyle}
         {...mouseEvents}
+        {...accessibility}        
       >
         {slides}
       </div>
